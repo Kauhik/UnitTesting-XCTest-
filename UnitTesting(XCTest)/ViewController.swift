5b7c20843f4509094b7082c23b11c78617897f66
“kauhik” <kaushikmanian456@gmail.com>
2025-10-17 15:41:15 +0700

    Initial Commit

---
+++ UnitTesting(XCTest)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTesting(XCTest)
+//
+//  Created by Kaushik Manian on 17/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UnitTesting(XCTest)Tests/UnitTesting_XCTest_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTesting_XCTest_Tests.swift
+//  UnitTesting(XCTest)Tests
+//
+//  Created by Kaushik Manian on 17/10/25.
+//
+
+import Testing
+@testable import UnitTesting_XCTest_
+
+struct UnitTesting_XCTest_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
